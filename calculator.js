3ef2b6cc34591c7aebada41a222132889f19fe6c
rick951 <rickning@pacificamerican.org>
2021-02-09 11:22:07 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-var result;
+let result;
 function calculateAdd (){
     var first = parseInt(document.querySelector('.first').value);
     var second = parseInt(document.querySelector('.second').value);
@@ -18,6 +18,13 @@ function calculateSubtract (){
 }function calculateDivide (){
     var first = parseInt(document.querySelector('.first').value);
     var second = parseInt(document.querySelector('.second').value);
-    result = first / second;
-    document.querySelector(".result").innerHTML = result;
+    var temp = first / second;
+    result = parseInt(temp);
+    if(first%second == 0){
+        document.querySelector(".result").innerHTML = result;
+    }else{
+        var modulus = first%second;
+        result = `${result} modulus: ${modulus}`;
+        document.querySelector(".result").innerHTML = result;
+    }
 }
